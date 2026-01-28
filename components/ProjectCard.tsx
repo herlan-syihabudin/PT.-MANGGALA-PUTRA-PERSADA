aecd41fa4856129d0c31256ac331f96642dc1b0d
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-28 11:56:45 +0700

    Update ProjectCard.tsx

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,32 +1,38 @@
+import Link from "next/link"
 import type { Project } from "@/lib/projects"
 
 export default function ProjectCard({
+  slug,
   image,
   category,
   title,
   description,
 }: Project) {
   return (
-    <div className="group border border-gray-200 rounded-2xl overflow-hidden bg-white hover:shadow-xl transition">
-      <div className="h-56 overflow-hidden">
-        <img
-          src={image}
-          alt={title}
-          className="w-full h-full object-cover group-hover:scale-105 transition duration-500"
-        />
-      </div>
+    <Link href={`/proyek/${slug}`} className="group">
+      <div className="border border-gray-200 rounded-2xl overflow-hidden bg-white hover:shadow-xl transition h-full">
+        
+        <div className="h-56 overflow-hidden">
+          <img
+            src={image}
+            alt={title}
+            className="w-full h-full object-cover group-hover:scale-105 transition duration-500"
+          />
+        </div>
 
-      <div className="p-6">
-        <span className="text-xs font-semibold text-red-600">
-          {category}
-        </span>
-        <h3 className="mt-2 text-xl font-bold text-gray-900">
-          {title}
-        </h3>
-        <p className="mt-3 text-gray-600 text-sm leading-relaxed">
-          {description}
-        </p>
+        <div className="p-6">
+          <span className="text-xs font-semibold text-red-600">
+            {category}
+          </span>
+          <h3 className="mt-2 text-xl font-bold text-gray-900">
+            {title}
+          </h3>
+          <p className="mt-3 text-gray-600 text-sm leading-relaxed">
+            {description}
+          </p>
+        </div>
+
       </div>
-    </div>
+    </Link>
   )
 }

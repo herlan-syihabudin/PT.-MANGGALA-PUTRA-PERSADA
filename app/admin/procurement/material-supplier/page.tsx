ef1914ec0c7e0bb9fc6948a0edf8014c55713b0b
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-02-04 20:29:36 +0700

    Create page.tsx

---
+++ app/admin/procurement/material-supplier/page.tsx
@@ -0,0 +1,9 @@
+import MaterialSupplierForm from "@/components/dashboard/master-data/MaterialSupplierForm"
+
+export default function MaterialSupplierPage() {
+  return (
+    <section className="p-6 md:p-10">
+      <MaterialSupplierForm />
+    </section>
+  )
+}

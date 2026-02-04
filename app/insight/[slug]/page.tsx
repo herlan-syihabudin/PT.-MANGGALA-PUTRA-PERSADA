1186d13880bbb050478cf753eed318135219321a
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-02-04 12:23:44 +0700

    Update page.tsx

--- app/insight/[slug]/page.tsx
+++ app/insight/[slug]/page.tsx
@@ -90,7 +90,35 @@ export default function InsightDetailPage({ params }: Props) {
           }),
         }}
       />
-
+<script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      itemListElement: [
+        {
+          "@type": "ListItem",
+          position: 1,
+          name: "Home",
+          item: "https://pt-manggala-putra-persada.vercel.app",
+        },
+        {
+          "@type": "ListItem",
+          position: 2,
+          name: "Insights",
+          item: "https://pt-manggala-putra-persada.vercel.app/insight",
+        },
+        {
+          "@type": "ListItem",
+          position: 3,
+          name: insight.title,
+          item: url,
+        },
+      ],
+    }),
+  }}
+/>
       <div className="max-w-3xl mx-auto px-6">
 
         {/* CATEGORY */}

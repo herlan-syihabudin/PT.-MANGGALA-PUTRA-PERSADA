a8a2c6e7efb1c6db33870ef3722a91a950d8e18e
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-02-04 11:58:31 +0700

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,25 @@ import { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
+        userAgent: "Bingbot",
+        allow: "/",
+      },
+      {
+        userAgent: "Yandex",
+        allow: "/",
+      },
+    ],
     sitemap: "https://pt-manggala-putra-persada.vercel.app/sitemap.xml",
+    host: "https://pt-manggala-putra-persada.vercel.app",
   }
 }

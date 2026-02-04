3df16156e82e5e0d5a4da6b9355c352050d84c6a
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-02-04 20:27:51 +0700

    Create page.tsx

---
+++ app/admin/system/master-material/page.tsx
@@ -0,0 +1,9 @@
+import MaterialForm from "@/components/dashboard/master-data/MaterialForm"
+
+export default function MasterMaterialPage() {
+  return (
+    <section className="p-6 md:p-10">
+      <MaterialForm />
+    </section>
+  )
+}

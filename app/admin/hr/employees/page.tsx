8a4c2fc51798d4ce9105d3a720513d24cff15540
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-02-05 11:02:07 +0700

    Create EmployeePage component for employee management

---
+++ app/admin/hr/employees/page.tsx
@@ -0,0 +1,16 @@
+export default function EmployeePage() {
+  return (
+    <section className="p-6 md:p-10">
+      <h1 className="text-3xl font-extrabold text-gray-900">
+        Master Karyawan
+      </h1>
+      <p className="text-gray-600 mt-1">
+        Data karyawan perusahaan
+      </p>
+
+      <div className="mt-6 bg-white border rounded-xl p-6">
+        🚧 Modul Employee sedang dikembangkan
+      </div>
+    </section>
+  )
+}

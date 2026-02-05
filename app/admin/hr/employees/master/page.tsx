240b3a96681344571bbf5a43a3882a440130d355
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-02-05 12:03:36 +0700

    Create EmployeeMasterPage component for employee management

---
+++ app/admin/hr/employees/master/page.tsx
@@ -0,0 +1,16 @@
+export default function EmployeeMasterPage() {
+  return (
+    <section className="p-6 md:p-10">
+      <h1 className="text-3xl font-extrabold text-gray-900">
+        Employee Master
+      </h1>
+      <p className="text-gray-600 mt-1">
+        Daftar & data inti karyawan
+      </p>
+
+      <div className="mt-6 bg-white border rounded-xl p-6">
+        🚧 Employee Master – tahap awal (list karyawan)
+      </div>
+    </section>
+  )
+}

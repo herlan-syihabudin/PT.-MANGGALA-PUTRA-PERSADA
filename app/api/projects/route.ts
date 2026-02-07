439a46ae6a9cc53af0051e5d8e5e8df81dff5bc0
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-02-07 17:04:55 +0700

    Update route.ts

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -15,7 +15,7 @@ const auth = new google.auth.JWT(
 
 const sheets = google.sheets({ version: "v4", auth })
 
-const SHEET_ID = process.env.GOOGLE_SHEET_ID!
+const SHEET_ID = process.env.GSHEET_PROJECT_ID!
 const SHEET_NAME = "PROJECT MASTER"
 
 /* ==============================

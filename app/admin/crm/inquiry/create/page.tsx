7ff33f72d4d4fddb469560644d3a85e915ab2a6a
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-02-11 16:28:36 +0700

    Update page.tsx

--- app/admin/crm/inquiry/create/page.tsx
+++ app/admin/crm/inquiry/create/page.tsx
@@ -86,7 +86,7 @@ export default function CreateInquiryPage() {
 
     setLoading(true)
 
-    const res = await fetch("/api/marketing/inquiry", {
+    const res = await fetch("/api/crm/inquiry", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({

66abf770c3c6973a461f904df08e2b389b2f8bef
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-02-19 17:51:11 +0700

    Delete app/admin/procurement/material-supplier/page.tsx

--- app/admin/procurement/material-supplier/page.tsx
+++
@@ -1,9 +0,0 @@
-import MaterialSupplierForm from "@/components/dashboard/master-data/MaterialSupplierForm"
-
-export default function MaterialSupplierPage() {
-  return (
-    <section className="p-6 md:p-10">
-      <MaterialSupplierForm />
-    </section>
-  )
-}

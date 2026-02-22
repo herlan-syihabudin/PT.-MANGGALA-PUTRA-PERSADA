4f037f7823616804f2b30879a596fa0d37fd5847
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-02-22 23:33:49 +0700

    Update page.tsx

--- app/admin/estimator/rab/[rab_id]/ve/page.tsx
+++ app/admin/estimator/rab/[rab_id]/ve/page.tsx
@@ -1,13 +1,7 @@
-// app/admin/estimator/rab/[rab_id]/ve/page.tsx
-
 import VEClient from "./VEClient"
 
 export const dynamic = "force-dynamic"
 
-export default function Page({
-  params,
-}: {
-  params: { rab_id: string }
-}) {
+export default function Page({ params }: { params: { rab_id: string } }) {
   return <VEClient rab_id={params.rab_id} />
 }

30f64efcf5d3b9e0ef4c5f5c62ca878728984ce6
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-03-03 00:13:53 +0700

    Update env.ts

--- lib/env.ts
+++ lib/env.ts
@@ -2,16 +2,22 @@ export function getEnv() {
   const {
     GOOGLE_CLIENT_EMAIL,
     GOOGLE_PRIVATE_KEY,
-    GOOGLE_SHEET_ID,
+    GSHEET_HR_ID,
+    GSHEET_CRM_ID,
+    GSHEET_ESTIMATOR_ID,
+    GSHEET_PROCUREMENT_ID,
   } = process.env
 
-  if (!GOOGLE_CLIENT_EMAIL || !GOOGLE_PRIVATE_KEY || !GOOGLE_SHEET_ID) {
-    throw new Error("Missing env variables")
+  if (!GOOGLE_CLIENT_EMAIL || !GOOGLE_PRIVATE_KEY) {
+    throw new Error("Missing Google credentials")
   }
 
   return {
     GOOGLE_CLIENT_EMAIL,
     GOOGLE_PRIVATE_KEY,
-    GOOGLE_SHEET_ID,
+    GSHEET_HR_ID: GSHEET_HR_ID || "",
+    GSHEET_CRM_ID: GSHEET_CRM_ID || "",
+    GSHEET_ESTIMATOR_ID: GSHEET_ESTIMATOR_ID || "",
+    GSHEET_PROCUREMENT_ID: GSHEET_PROCUREMENT_ID || "",
   }
 }

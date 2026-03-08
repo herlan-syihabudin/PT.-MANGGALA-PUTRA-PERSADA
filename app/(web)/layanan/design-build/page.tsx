6af21d1e703a6fe6539b2c385457e49f62cda328
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-03-08 23:16:26 +0700

    Update page.tsx

--- app/(web)/layanan/design-build/page.tsx
+++ app/(web)/layanan/design-build/page.tsx
@@ -77,7 +77,7 @@ dari tahap perencanaan hingga serah terima pekerjaan.
           {/* IMAGE */}
           <div className="relative h-[240px] md:h-[320px] rounded-2xl overflow-hidden bg-gray-200 shadow-xl">
             <Image
-              src="/images/design-build-construction-site.jpg"
+              src="/insigths/images/design-build-construction-site.jpg"
               alt="Design build contractor industrial construction project Indonesia"
               fill
               priority
